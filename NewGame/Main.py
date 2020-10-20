43802d9835fae5ac5201478ffbfa16fff1c57f6f
isidars <54060555+isidars@users.noreply.github.com>
2020-10-20 22:09:07 +0200

    Nouvelle class de monstre avec un monstre test "dragon" et import de la classe dans le fichier Main.py

--- NewGame/Main.py
+++ NewGame/Main.py
@@ -1,4 +1,7 @@
 # Fichier contenant le code principal du jeu
+# On importe la class Monster
+from Monster import *
+
 print("Bienvenue dans cette aventure!")
 
-
+print("Vous êtes attaqué par un monstre, c'est un "+Monster.name+"!\nIl a "+str(Monster.monster_life)+" point de vie.\nBonne chance !")
---
+++ NewGame/Monster.py
@@ -0,0 +1,6 @@
+#Fichier contenant tous les monstres et leur caracteristiques
+
+class Monster:
+    name = "dragon"
+    monster_life = 100
+    monster_life_max = 100
