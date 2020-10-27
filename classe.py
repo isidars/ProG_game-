b4993b5e06909aba4ee9858ed15e564f84737fca
Xientras-dev <73475433+Xientras-dev@users.noreply.github.com>
2020-10-27 08:28:38 +0100

    Add files via upload

---
+++ classe.py
@@ -0,0 +1,15 @@
+#Déclaration des variables
+vie
+force
+stamina
+précision
+vitesse
+résistance
+magie
+
+#Déclaration des différentes classes
+tank -> vie+ & résistance+
+mage -> magie+
+assassin -> vitesse+ & force+
+archer
+guerrier
