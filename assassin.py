7cf7a2ac110174efe37cb81149a69a649f098eae
NeoNyso <72974757+NeoNyso@users.noreply.github.com>
2020-11-10 18:32:43 +0100

    Add files via upload

---
+++ assassin.py
@@ -0,0 +1,20 @@
+# Classe joueur
+class Joueur:
+    def init(self, nom, role):
+        
+self.nom = nom
+self.role = role
+self.etat = "normal"
+self.pv = 100
+self.pm = 100
+self.endurance = 100
+self.attaque = 100
+self.attaque_magique =  150
+self.defense = 50
+self.defense_magique = 60
+self.vitesse = 130
+self.precision = 110
+self.esquive = 100
+self.experience = 0
+self.niveau = 1
+
