5d51e04ddb49859c6aee520bbb7ea1b1f058aaa3
franciys <72937846+franciys@users.noreply.github.com>
2020-11-17 21:08:01 +0100

    Add files via upload

---
+++ NewGame/Paladin.py
@@ -0,0 +1,24 @@
+
+PaladinStat={			"Name":"Paladin",
+						"PV":"120",
+						"PM":"80",
+						"EDRC":"120",
+						"ATK":"100",
+						"ATK_MAG":"80",
+						"DEF":"120",
+						"DEF_MAG":"120",
+						"VIT":"80",
+						"PREC":"100",
+						"ESQV":"80",
+											
+						#"TAUX_CRITIQUE":
+						#"ETAT":
+						#"EXPERIENCE":
+						#"NIVEAU":										
+						}
+
+PaladinComp=["Frappe","Charge","Contre","Marteau"]
+PaladinMagie=["Bouclier","Defense","Recupération"]
+
+PaladinLvl=1
+
