0bd86f8cfe86df5ec4b46c83f549275100e4e0aa
franciys <72937846+franciys@users.noreply.github.com>
2020-11-18 20:20:28 +0100

    Add files via upload

---
+++ NewGame/Personnages.py
@@ -0,0 +1,27 @@
+class Paladin():
+
+	def __init__ (self):
+
+		self.Name="Paladin"
+		self.PV=120
+		self.PM=80
+		self.EDRC=120
+		self.ATK=100
+		self.ATK_MAG=80
+		self.DEF=120
+		self.DEF_MAG=120
+		self.VIT=80
+		self.PREC=100
+		self.ESQV=80
+											
+		#"TAUX_CRITIQUE"=
+		#"ETAT"=
+		#"EXPERIENCE"=
+		#"NIVEAU"=										
+		
+
+PaladinComp=["Frappe","Charge","Contre","Marteau"]
+PaladinMagie=["Bouclier","Defense","Recupération"]
+
+PaladinLvl=1
+
